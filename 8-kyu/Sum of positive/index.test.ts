5f98ade8e2712ab6c52629a0293d61b72ba06e89
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-08-31 11:00:00 +0200

    feat(8-kyu): sum of positive

---
+++ 8-kyu/Sum of positive/index.test.ts
@@ -0,0 +1,11 @@
+import { positiveSum } from '.';
+
+describe('solution', function () {
+  it('Basic Tests', function () {
+    expect(positiveSum([1, 2, 3, 4, 5])).toBe(15);
+    expect(positiveSum([1, -2, 3, 4, 5])).toBe(13);
+    expect(positiveSum([])).toBe(0);
+    expect(positiveSum([-1, -2, -3, -4, -5])).toBe(0);
+    expect(positiveSum([-1, 2, 3, 4, -5])).toBe(9);
+  });
+});
