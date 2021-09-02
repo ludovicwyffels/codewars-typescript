df493ae7b000e5e211ac6748ff6008bb2013c56f
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-09-02 10:15:00 +0200

    feat(8-kyu): beginner series 2 clock

---
+++ 8-kyu/Beginner Series 2 Clock/index.test.ts
@@ -0,0 +1,11 @@
+import { past } from '.';
+
+describe('Fixed Tests', () => {
+  it('Tests', () => {
+    expect(past(0, 1, 1)).toBe(61000);
+    expect(past(1, 1, 1)).toBe(3661000);
+    expect(past(0, 0, 0)).toBe(0);
+    expect(past(1, 0, 1)).toBe(3601000);
+    expect(past(1, 0, 0)).toBe(3600000);
+  });
+});
