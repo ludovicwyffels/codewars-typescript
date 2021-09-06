063465be9037698d759588f79e0a774ef502304e
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-09-06 10:39:17 +0200

    feat(8-kyu): convert a string to a number

---
+++ 8-kyu/Convert a String to a Number/index.test.ts
@@ -0,0 +1,10 @@
+import { stringToNumber } from '.';
+
+describe('stringToNumber', function () {
+  it('should work for the examples', function () {
+    expect(stringToNumber('1234')).toBe(1234);
+    expect(stringToNumber('605')).toBe(605);
+    expect(stringToNumber('1405')).toBe(1405);
+    expect(stringToNumber('-7')).toBe(-7);
+  });
+});
